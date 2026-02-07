5083def0f252fbf09d6f5e2eee749f2230652dda
Cole Williams <coletnwilliams@gmail.com>
2026-02-06 17:47:21 -0800

    Create Main Pages
    Added code to the other main pages. Created a Volunteer Page HTML file. Added automatic copyright year.

---
+++ script.js
@@ -0,0 +1,4 @@
+// Set current year in footer
+//document.getElementByClassName('spanYear')[0].textContent = new Date().getFullYear();
+
+document.getElementById('spanYear').textContent = new Date().getFullYear();
